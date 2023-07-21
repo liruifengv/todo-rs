afa6b546c9feada6c8b2f0c4144ddafbcd1f38c4
liruifengv <liruifeng1024@gmail.com>
2023-07-21 16:22:50 +0800

    It's working!

---
+++ src/database.rs
@@ -0,0 +1,97 @@
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Seek, Write};
+// 数据记录
+pub struct Record {
+    pub id: i32,
+    pub content: String,
+}
+
+// 解析记录行
+pub fn parse_record_line(line: &str) -> Record {
+    let fields: Vec<&str> = line.split(',').collect();
+    // 处理空行的情况
+    if fields.len() == 1 {
+        return Record {
+            id: 0,
+            content: "".to_string(),
+        };
+    }
+    Record {
+        id: fields[0].parse::<i32>().unwrap(),
+        content: fields[1].to_string(),
+    }
+}
+
+// 数据库
+pub struct Database {
+    pub file: File,
+}
+
+impl Database {
+    // 打开数据库文件
+    pub fn open(filename: &str) -> Database {
+        let file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .open(filename)
+            .unwrap();
+        Database { file }
+    }
+
+    // 添加记录
+    pub fn add_record(&mut self, record: &Record) {
+        let line = format!("{},{}\n", record.id, record.content);
+        writeln!(self.file, "{}", line).unwrap();
+        println!("📝 Item added: {}", record.content);
+    }
+
+    // 删除记录
+    pub fn remove_record(&mut self, id: i32) {
+        let reader = BufReader::new(&self.file);
+        let mut lines = reader.lines().enumerate();
+        let line = lines.find(|(_, line)| {
+            let record = parse_record_line(&line.as_ref().unwrap());
+            record.id == id
+        });
+        match line {
+            Some((i, _)) => {
+                let contents = fs::read_to_string(".rodorc").unwrap();
+                let mut new_contents = String::new();
+                for (j, line) in contents.lines().enumerate() {
+                    if i != j {
+                        new_contents.push_str(line);
+                    }
+                }
+                self.file.seek(std::io::SeekFrom::Start(0)).unwrap();
+                self.file.write_all(new_contents.as_bytes()).unwrap();
+                self.file.set_len(new_contents.len() as u64).unwrap();
+
+                println!(" ❌ Item removed!\n");
+            }
+            None => {
+                println!("No such record: {}", id);
+            }
+        }
+    }
+
+    // 读取记录
+    pub fn read_records(&mut self) -> Vec<Record> {
+        let reader = BufReader::new(&self.file);
+        let mut records = Vec::new();
+        for line in reader.lines() {
+            match line {
+                Ok(line) => {
+                    if line == "" {
+                        continue;
+                    }
+                    let record = parse_record_line(&line);
+                    records.push(record);
+                }
+                Err(_) => panic!("Error reading file"),
+            }
+        }
+        records
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::env;
+mod database;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: rodo [add|rm|ls] [args]");
+        return;
+    }
+
+    let mut db = database::Database::open(".rodorc");
+
+    let command = &args[1];
+    match command.as_str() {
+        "add" => {
+            if args.len() < 3 {
+                println!("Usage: rodo add [contents]");
+                return;
+            }
+            let contents = &args[2..].join(" ");
+            let len = db.read_records().len();
+            db.add_record(&database::Record {
+                id: len as i32 + 1,
+                content: contents.to_string(),
+            });
+        }
+        "rm" => {
+            if args.len() < 3 {
+                println!("Usage: rodo rm [id]");
+                return;
+            }
+            let id = args[2].parse::<i32>().unwrap();
+            db.remove_record(id);
+        }
+        "ls" => {
+            let records = db.read_records();
+            if records.len() == 0 {
+                println!("No records. You can add one with `rodo add [content]`");
+                return;
+            }
+            for record in records {
+                println!(" ⬜️ {}: {}", record.id, record.content);
+            }
+        }
+        _ => {
+            println!("Unknown command: {}", command);
+        }
+    }
 }
